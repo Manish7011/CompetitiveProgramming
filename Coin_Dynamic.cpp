13f6e6a0e5e6c83f692e2b98bb6b2496a0ffa649
Manish Bhadani <Manish.Bhadani@FreedomMortgage.com>
2019-04-25 07:50:32 +0530

    Create Coin_Dynamic.cpp

---
+++ Coin_Dynamic.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+int recursive_coin(int);
+int memoize_coin(int);
+void iterative_coin(int);
+void print_iterative_coin(int);
+int main(){
+    print_iterative_coin(12589);
+    return 0;
+}
+int coins[] = {1,2,5,10,20,50,100,200,500};
+int check[10000];
+bool flag[10000];
+
+void print_iterative_coin(int N){
+    int V[N+1];
+    int first[N+1];
+    V[0] = 0;
+    for(int x=1;x<=N;x++){
+        V[x] = INT_MAX-1;
+        for(auto coin : coins){
+            if(x-coin >= 0 && V[x-coin]+1 < V[x]){
+                V[x] = V[x-coin]+1;
+                first[x] = coin;
+            }
+        }
+    }
+    cout<<"Total Coin: "<<V[N]<<endl;
+    cout<<"Used Coins: "<<endl;
+    while(N>0){
+        cout<<first[N]<<" ";
+        N-=first[N];
+    }
+    return;
+}
+void iterative_coin(int N){
+    int V[N+1];
+    V[0] = 0;
+    for(int x=1;x<=N;x++){
+        V[x] = INT_MAX-1;
+        for(auto coin : coins){
+            if(x-coin >= 0){
+                V[x] = min(V[x],V[x-coin]+1);
+            }
+        }
+    }
+    cout<<"Total Coin : "<<V[N]<<endl;
+    return;
+}
+int memoize_coin(int remaining){
+    if(remaining<0){
+        return INT_MAX-1;
+    }
+    if(remaining==0){
+        return 0;
+    }
+    if(flag[remaining]){
+        return check[remaining];
+    }
+    int best = INT_MAX-1;
+    for(auto coin:coins){
+        best = min(best, recursive_coin(remaining-coin)+1);
+    }
+    check[remaining] = best;
+    flag[remaining] = true;
+
+    return best;
+}
+
+int recursive_coin(int remaining){
+    if(remaining<0){
+        return INT_MAX-1;
+    }
+    if(remaining==0){
+        return 0;
+    }
+    int best = INT_MAX-1;
+    for(auto coin:coins){
+        best = min(best, recursive_coin(remaining-coin)+1);
+    }
+    return best;
+}
